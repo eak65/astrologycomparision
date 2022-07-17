34939ac2a27f23fb1078cb5538b99a65ecf4c68e
Ethan <keiserea@gmail.com>
2022-07-17 11:00:20 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class ComparisionForm extends React.Component {
         var age1 = parseInt(this.state.age1);
         var age2 = parseInt(this.state.age2);
 
-        ageString += this.state.as2 + "'s have a tendency to cherish and connect with " + this.state.as1 +"'s that are between the ages of " + (age1 - 2) +" - " + (age1+ 2)+"."
+        ageString += this.state.as2 + "'s have a tendency to cherish and connect with " + this.state.as1 +"'s that are between the ages of " + (age1 - 2) +" - " + (age1+ 2)+". "
         ageString += this.state.as1 + "'s are most compatible with " + this.state.as2 +"'s that are between the ages of " + (age2 - 2) +" - " + (age2 + 1)+"."
 
       } else if (this.state.age1 != '' && this.state.age2 == '') {
@@ -69,19 +69,18 @@ class ComparisionForm extends React.Component {
       }
       ageString += " This is because their Chinese Lunar Year signs are universally aligned. "
 
-      hairString += " Research shows that certain astrological signs are attracted"
+      hairString += " Research shows that certain astrological signs are attracted various hair colors. "
       if (this.state.hairColor1 != '' && this.state.hairColor2 != '') { 
+        hairString += this.state.as1 + "'s tend like " + this.state.hairColor2 + " hair and " + this.state.as2 + " more often like " + this.state.hairColor1 + " hair."
       }
-      else if (this.state.age1 != '' && this.state.age2 == '') {
+      else if (this.state.hairColor1 != '' && this.state.hairColor2 == '') {
+        hairString += this.state.as2 + "'s tend like " + this.state.his.state.hairColor1 + " hair.";
       }
-      else if (this.state.age1 == '' && this.state.age2 != '') {
+      else if (this.state.hairColor1 == '' && this.state.hairColor2 != '') {
+        hairString += this.state.as1 + "'s tend like " + this.state.his.state.hairColor2 + " hair.";
       }
 
-
- 
-
-
-      this.setState({textarea: "Both " + this.state.as1 + " and "+this.state.as2 + " are extremely compatible. " + ageString});
+      this.setState({textarea: "Both " + this.state.as1 + " and "+this.state.as2 + " are extremely compatible. " + ageString + hairString});
       event.preventDefault();
   }
   post(event) {
@@ -106,129 +105,133 @@ class ComparisionForm extends React.Component {
 
   render() {
     return (
-      <div class="container">
+      <div className="container">
       <div>Welcome to Astrology Compatability creator!</div>
       <form onSubmit={this.handleSubmit}>
         <p>Your information:</p>
-        <div class="row"> 
-            <div class="col">
+        <div className="row"> 
+            <div className="col">
             <label>
             Astrological Sign: 
             </label>
             </div>
-            <div class="col">
+            <div className="col">
 
             <select value={this.state.as1} name="as1" onChange={this.handleChange}>
               <option value="Aries">Aries</option>
               <option value="Taurus">Taurus</option>
-              <option selected value="Gemini">Gemini</option>
+              <option value="Gemini">Gemini</option>
               <option value="Cancer">Cancer</option>
               <option value="Leo">Leo</option>
               <option value="Virgo">Virgo</option>
-              <option selected value="Libra">Libra</option>
+              <option value="Libra">Libra</option>
               <option value="Scorpio">Scorpio</option>
               <option value="Sagittarius">Sagittarius</option>
               <option value="Capricorn">Capricorn</option>
               <option value="Aquarius">Aquarius</option>
-              <option selected value="Pisces">Pisces</option>
+              <option value="Pisces">Pisces</option>
             </select> 
             </div>
           </div>
-            <div class="row"> 
-            <div class="col">
+            <div className="row"> 
+            <div className="col">
             <label>
             Age:     </label>
             </div>
-            <div class="col">
+            <div className="col">
               <input type="number" min="13" max="100" value={this.state.age1} name="age1"  onChange={this.handleChange} />
           </div>
           </div>
-          <div class="row"> 
-            <div class="col">
+          <div className="row"> 
+            <div className="col">
             <label>
             Hair Color:</label>
             </div>           
-             <div class="col">
+             <div className="col">
              <select value={this.state.hairColor1} name="hairColor1" onChange={this.handleChange}>
-             <option disabled value = '' selected></option>
-              <option value="Brown">Brown</option>
-              <option value="Blonde">Blonde</option>
-              <option value="Red">Red</option>
-              <option value="Other">Other</option>
+             <option disabled value = ''></option>
+             <option value="brown">Brown</option>
+              <option value="blonde">Blonde</option>
+              <option value="red">Red</option>
+              <option value="blue">Blue</option>
+              <option value="grey">Grey</option>
+              <option value="no">Bald</option>
             </select>
             </div>
           </div>
-          <div class="row"> 
-          <div class="col-12">
+          <div className="row"> 
+          <div className="col-12">
         <p>Their information:</p>
           </div>
         </div>
-        <div class="row"> 
-            <div class="col">
+        <div className="row"> 
+            <div className="col">
 
             <label>
             Astrological Sign:
             </label>
             </div>
-            <div class="col">
+            <div className="col">
             <select value={this.state.as2} name="as2" onChange={this.handleChange}>
             <option value="Aries">Aries</option>
               <option value="Taurus">Taurus</option>
-              <option selected value="Gemini">Gemini</option>
+              <option value="Gemini">Gemini</option>
               <option value="Cancer">Cancer</option>
               <option value="Leo">Leo</option>
               <option value="Virgo">Virgo</option>
-              <option selected value="Libra">Libra</option>
+              <option value="Libra">Libra</option>
               <option value="Scorpio">Scorpio</option>
               <option value="Sagittarius">Sagittarius</option>
               <option value="Capricorn">Capricorn</option>
               <option value="Aquarius">Aquarius</option>
-              <option selected value="Pisces">Pisces</option>
+              <option value="Pisces">Pisces</option>
             </select>
             </div>
             </div>
-          <div class="row"> 
-            <div class="col">
+          <div className="row"> 
+            <div className="col">
             <label>
             Age:</label>
             </div>
-            <div class="col">
+            <div className="col">
             <input type="number" min="13" max="100" value={this.state.age2} name="age2"  onChange={this.handleChange} />
             </div>
             </div>
             
-            <div class="row"> 
-            <div class="col">
+            <div className="row"> 
+            <div className="col">
             <label>
             Hair Color:</label>
             </div>
-            <div class="col">
+            <div className="col">
             <select value={this.state.hairColor2} name="hairColor2" onChange={this.handleChange}>
-            <option disabled value = '' selected></option>
-            <option value="Brown">Brown</option>
-              <option value="Blonde">Blonde</option>
-              <option value="Red">Red</option>
-              <option value="Other">Other</option>
+            <option disabled value = ''></option>
+            <option value="brown">Brown</option>
+              <option value="blonde">Blonde</option>
+              <option value="red">Red</option>
+              <option value="blue">Blue</option>
+              <option value="grey">Grey</option>
+              <option value="no">Bald</option>
             </select>
             </div>
             </div>
-            <div class="row"> 
-            <div class="col-3 mt-5 offset-3 justify-content-center">
+            <div className="row"> 
+            <div className="col-3 mt-5 offset-3 justify-content-center">
               <input value ="Generate Comparision" type="submit" />
         </div>
         </div>
 
       </form>
       <br/>
-      <div class='row'>
-      <div class='col-9 offset-0'>
+      <div className='row'>
+      <div className='col-9 offset-0'>
 
       <form onSubmit={this.post}>
         <label>Results:</label>
         <br/>
         <br/>
-        <div class='row'>
-          <div class='col-12'>
+        <div className='row'>
+          <div className='col-12'>
         <textarea rows="10" style={{width: "100%"}} value={this.state.textarea}  name="textarea" placeholder='Generated comparisions will appear here...' onChange={this.handleChange} />
        </div>
        </div>
--- src/Comparision.js
+++ src/Comparision.js
@@ -1,42 +1,95 @@
 import React, { useEffect, useState } from "react";
 import {useParams, useLocation} from "react-router-dom";
 
-import leo from './leo.jpg';
-import libra from './libra.jpg';
+import aquarius from './images/aquarius.jpg';
+import aries from './images/aries.jpg';
+import cancer from './images/cancer.jpg';
+import capricorn from './images/capricorn.jpg';
+import gemini from './images/gemini.jpg';
+import leo from './images/leo.jpg';
+import libra from './images/libra.jpg';
+import pisces from './images/pisces.jpg';
+import sagittarius from './images/sagittarius.jpg';
+import scorpio from './images/scorpio.jpg';
+import taurus from './images/taurus.jpg';
+import virgo from './images/virgo.jpg';
 
 
   function Result() {
     // Initialize state to hold the posts
     const [posts, setPosts] = useState([]);
     const id = useParams().id
-  
+    const [image1, setImage1] = useState([]);
+    const [image2, setImage2] = useState([]);
+
+    var loaded = false;
     // effect functions can't be async, so declare the
     // async function inside the effect, then call it
+    console.log("render")
+
     useEffect(() => {
       async function fetchData() {
         // Call fetch as usual
+        console.log("begin load")
+
         const res = await fetch('https://84dvvklokj.execute-api.us-east-1.amazonaws.com/Production/compatibility?id='+id);
   
         // Pull out the data as usual
         const json = await res.json();
-  
         // Save the posts into state
         // (look at the Network tab to see why the path is like this)
+        console.log("save response")
+
+        console.log("load images ")
+        setImage1(getImage(posts.as1));
+        console.log(image1)
+        setImage2(getImage(posts.as2));
         setPosts(json);
+
       }
-  
+      function getImage(value) {
+        var image;
+        if (value == "Aquarius") {
+            image = aquarius
+        } else if (value == "Aries") {
+            image = aries
+        } else if (value == "Cancer") {
+            image = cancer
+        } else if (value == "Capricorn") {
+            image = capricorn
+        } else if (value == "Gemini") {
+            image = gemini
+        } else if (value == "Leo") {
+            image = leo
+        } else if (value == "Libra") {
+            image = libra
+        } else if (value == "Pisces") {
+            image = pisces
+        } else if (value == "Sagittarius") {
+            image = sagittarius
+        } else if (value == "Scorpio") {
+            image = scorpio
+        } else if (value == "Taurus") {
+            image = taurus
+        } else if (value == "Virgo") {
+            image = virgo
+        } else {
+            console.log(value)
+            console.log("nothing")
+        }
+        return image;
+      }
       fetchData();
-    }); // <-- we didn't pass a value. what do you think will happen?
-  
-    // Render as usual
+      loaded = true;
+    },[image2]); // <-- we didn't pass a value. what do you think will happen?
     return (
         <div>
       <div className="row">
         <div className="col-6 d-block m-auto">
-        <img className="Circular" src={leo}  />
+        <img className="Circular" src={image1}  />
         </div>
         <div className="col-6 ">
-      { <img className="Circular" src={libra}  />}
+      { <img className="Circular" src={image2}  />}
       </div>
       </div>
       <div className="row mt-5">
